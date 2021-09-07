1cb826c9e9bb98e7bc38d711a74116a554fa3ea4
aliverise@naver.com <aliverise@naver.com>
2021-09-07 21:45:11 +0900

    2021.09.07. 문제풀어서 하나올리기

--- RyuKiTak/src/M09/D01_최대공약수와최소공배수.java
+++ RyuKiTak/src/M09/D01_최대공약수와최소공배수.java
@@ -3,7 +3,9 @@ package M09;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 // https://www.acmicpc.net/problem/2609
+// 20210907
 public class D01_최대공약수와최소공배수 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
